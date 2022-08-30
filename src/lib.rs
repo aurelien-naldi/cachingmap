840dc3953e99bb8ba1b606406c564ceabd3fccac
Aurelien Naldi <aurelien.naldi@gmail.com>
2022-08-30 13:45:31 +0200

    Simplify into a LazyMap carying an inner closure

--- src/lib.rs
+++ src/lib.rs
@@ -1,116 +1,82 @@
-//! This crate defines the [CachingMap] trait: a map accepting insertion of **new** entries while immutable.
+//! A [LazyMap] adds caching to a mapping closure to call it only once on each key and only if needed.
 //!
-//! The most convenient use of the caching map is to request a cached value with a closure to compute it if needed.
-//! The closure will be called only if the key is missing from the cache. Note that only the first call will execute
-//! the closure, all following calls will use the cache, even if the closure is different or not predictable.
+//! The closure is called only if the key is missing from the cache. Note that only the first call will execute the closure,
+//! all following calls with the same key will then use the cache, even if the output of the closure is not stable.
 //!
 //! ```
-//! use cachingmap::*;
+//! use cachingmap::LazyMap;
 //!
-//! // Suppose that we have an expensive function returning predictable results
+//! // Suppose that we have an expensive function returning predictable results (without side effects)
 //! fn compute_value(seed: &usize) -> String // content skipped
 //! # {   format!("Computed for seed {}", *seed) }
 //! # let (comp1, comp2, comp10) = (compute_value(&1), compute_value(&2), compute_value(&10));
 //!
 //! // Create a cache and use closures to compute and cache the result
-//! let mut cache = AsyncCachingMap::default();
-//! let ref1 = cache.cache(1, |v| compute_value(v));
-//! let ref3 = cache.cache(3, compute_value);
+//! let mut cache = LazyMap::new(|k| compute_value(&k));
+//! let ref1 = cache.get(1);
+//! let ref3 = cache.get(3);
 //!
 //! // If we call it on an existing key, the closure is NOT executed
 //! // and we obtain a reference to the previously cached object
-//! let ref1b = cache.cache(1, |v| compute_value(v));    // same result, skipped
-//! let ref1c = cache.cache(1, |v| compute_value(&10));   // different result, also skipped
+//! let ref1b = cache.get(1,);    // same result, skipped
 //!
-//! // Only the first inserted a value in the cache. All references borrow the same data
-//! assert!(ref1.is_new() && ref1b.is_old() && ref1c.is_old());
-//! assert!(std::ptr::eq(*ref1, *ref1c));
-//! # assert_eq!(*ref1, &comp1);
-//! # assert_eq!(*ref1b, &comp1);
-//! # assert_ne!(*ref1b, &comp10);
+//! // Only the first inserted a value is stored in the cache. All references then borrow the same data
+//! assert!(std::ptr::eq(ref1, ref1b));
+//! # assert_eq!(ref1, &comp1);
+//! # assert_eq!(ref1b, &comp1);
+//! # assert_ne!(ref1b, &comp10);
 //!
-//! // Any mutable access to the cache invalidates previous references.
+//! // Any mutable access to the cache invalidates all previously returned references.
 //! // This allows to clear the full cache, remove or replace individual entries, ...
 //! cache.remove(&1);
-//! let ref1d = cache.cache(1, |v| compute_value(&10));
+//! let ref1c = cache.get(1);
 //!
 //! // The borrow checker now rejects the use of any previously returned references
-//! // ref1.is_new();  // Does NOT compile after the call to remove
-//! # assert_eq!(*ref1d, &comp10);
+//! // (e.g. ref1  is NOT ACCESSIBLE after the call to remove).
+//! # assert_eq!(ref1c, &comp1);
 //! ```
-use std::borrow::Cow;
-use std::ops::Deref;
+use std::cell::UnsafeCell;
+use std::collections::HashMap;
+use std::hash::Hash;
 
-mod boxed;
-
-pub use boxed::AsyncCachingMap;
-
-/// A Map accepting immutable insertion of new entries.
+/// Lazy value mapping based on a closure and a caching map to store.
 ///
-/// See the [crate doc](crate) for a simple example.
+/// Map values are lazily computed using a closure on the first access and
+/// then stored in a caching hashmap for later access.
 ///
-/// # Finer control of the cache
-///
-/// If some results can be obtained efficiently and without new allocation, we may want to avoid wasting memory by caching them.
-/// The [CachingMap::cache_owned] method takes a closure returning a [Cow] object, it will only cache the Owned results.
-pub trait CachingMap<K, V: Clone> {
-    /// Retrieve an entry from the cache and use a closure to compute and add it if missing.
-    ///
-    /// If the key is not in the cache, execute the closure to get the value and add cache it.
-    /// return a reference to the cached value (existing or newly added)
-    ///
-    /// Note that if a cached value exists, the closure is ignored.
-    /// Calling this with different closures or closures that do not always return the same value
-    /// can give unexpected results.
-    fn cache<F: Fn(&K) -> V>(&self, key: K, f: F) -> CachedValue<V> {
-        self.cache_owned(key, |k| Cow::Owned(f(k)))
-    }
-
-    /// Retrieve an entry from the cache and use a closure to compute and add it if missing
-    ///
-    /// This is a variant of [Self::cache] with similar properties:
-    /// if the key is in the cache,the value in cache will be returned immediately,
-    /// otherwise, the closure is used to get a value. In this variant, the value obtained with
-    /// the closure is a [Cow] object, which can be either owned or borrowed.
-    /// Borrowed values are returned without updating the cache.
-    /// Owned values are cached before returning an internal reference.
-    fn cache_owned<'a, F: Fn(&K) -> Cow<'a, V>>(&'a self, key: K, f: F) -> CachedValue<'a, V>;
+/// Uses unsafe code for interior mutability as we need to expose references inside an UnsafeCell.
+pub struct LazyMap<K, V> {
+    f: fn(K) -> V,
+    cache: UnsafeCell<HashMap<K, Box<V>>>,
 }
 
-/// A reference provided by the cache
-///
-/// The variants are used to reflect the state of the cache when it has been obtained.
-/// All variants carry a reference to the same type of object, available through [Deref].
-#[derive(Copy, Clone, Debug, Eq, PartialEq)]
-pub enum CachedValue<'a, T> {
-    /// The reference has been forwarded from another owner
-    Ext(&'a T),
-    /// The reference has just been added to the cache
-    New(&'a T),
-    /// The reference was already in the cache
-    Old(&'a T),
-}
-
-impl<T> CachedValue<'_, T> {
-    pub const fn is_ext(&self) -> bool {
-        matches!(*self, Self::Ext(_))
-    }
-    pub const fn is_new(&self) -> bool {
-        matches!(*self, Self::New(_))
-    }
-    pub const fn is_old(&self) -> bool {
-        matches!(*self, Self::Old(_))
-    }
-}
-
-impl<'a, T> Deref for CachedValue<'a, T> {
-    type Target = &'a T;
-
-    fn deref(&self) -> &Self::Target {
-        match self {
-            Self::Ext(v) => v,
-            Self::New(v) => v,
-            Self::Old(v) => v,
+impl<K: Eq + Hash + Copy, V> LazyMap<K, V> {
+    /// Create a LazyMap using a closure
+    pub fn new(f: fn(K) -> V) -> Self {
+        Self {
+            f,
+            cache: UnsafeCell::default(),
         }
+    }
+
+    pub fn get(&self, key: K) -> &V {
+        // Get unsafe mutable access to the caching map.
+        // This operation is safe as long as references to the existing content remain unmodified.
+        // Here it holds as we only add new values and boxing ensure stability of the references.
+        unsafe { self.cache.get().as_mut().unwrap() }
+            .entry(key)
+            .or_insert_with(|| Box::new((self.f)(key)))
+    }
+
+    pub fn remove(&mut self, key: &K) -> Option<Box<V>> {
+        self.cache.get_mut().remove(key)
+    }
+
+    pub fn clear(&mut self) {
+        self.cache.get_mut().clear()
+    }
+
+    pub fn len(&self) -> usize {
+        unsafe { self.cache.get().as_ref().unwrap() }.len()
     }
 }
